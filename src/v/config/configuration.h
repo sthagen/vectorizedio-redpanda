redpanda-data/redpanda

memory_groups: Introduce reserve for partitions
Static partition memory use is currently unaccounted for in memory
groups.

This patch introduces a reserve to account for partition memory usage.

We don't use a share like some of the other groups but rather an upfront
percentage based. This is because we don't want the partitions share to
shrink once we add more groups.

src/v/config/configuration.h
@@ -156,6 +156,7 @@ struct configuration final : public config_store {
     bounded_property<std::optional<int32_t>> topic_fds_per_partition;
     bounded_property<uint32_t> topic_partitions_per_shard;
     bounded_property<uint32_t> topic_partitions_reserve_shard0;
+    bounded_property<uint32_t> topic_partitions_max_memory_allocation_share;
     property<std::chrono::milliseconds>
       partition_manager_shutdown_watchdog_timeout;
 

sthagen/vectorizedio-redpanda

src/v/config/configuration.h
@@ -156,6 +156,7 @@ struct configuration final : public config_store {
     bounded_property<std::optional<int32_t>> topic_fds_per_partition;
     bounded_property<uint32_t> topic_partitions_per_shard;
     bounded_property<uint32_t> topic_partitions_reserve_shard0;
+    bounded_property<uint32_t> topic_partitions_memory_allocation_percent;
     property<std::chrono::milliseconds>
       partition_manager_shutdown_watchdog_timeout;
 