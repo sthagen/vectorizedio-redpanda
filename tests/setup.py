011d61a755755a9d86c32744a0246b5ef5945d0f
Alexey Bashtanov <alexey.bashtanov@redpanda.com>
2025-01-08 10:31:44 +0000

    tests/setup: bump ducktape version to log node IPs
    https://github.com/redpanda-data/ducktape/pull/40

--- tests/setup.py
+++ tests/setup.py
@@ -13,7 +13,7 @@ setup(
     package_data={'': ['*.md']},
     include_package_data=True,
     install_requires=[
-        'ducktape@git+https://github.com/redpanda-data/ducktape.git@f51d83f1c035cf77f9389139646f5abdaa6d3648',
+        'ducktape@git+https://github.com/redpanda-data/ducktape.git@b8a491106929d9dcf4d9db154feba0f14298bdd3',
         'prometheus-client==0.9.0',
         'kafka-python==2.0.2',
         'crc32c==2.2',
