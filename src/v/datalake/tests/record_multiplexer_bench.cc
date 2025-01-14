feeff1ddbadb4d1a4de96aaea1f4107d24578a8f
Tyler Rockwood <rockwood@redpanda.com>
2025-01-14 16:47:34 +0000

    bazel/datalake: add benchmarks

--- src/v/datalake/tests/record_multiplexer_bench.cc
+++ src/v/datalake/tests/record_multiplexer_bench.cc
@@ -23,9 +23,6 @@
 #include "serde/protobuf/tests/data_generator.h"
 
 #include <seastar/testing/perf_tests.hh>
-#include <seastar/testing/thread_test_case.hh>
-
-#include <boost/test/tools/old/interface.hpp>
 
 #include <optional>
 #include <string_view>
@@ -102,7 +99,7 @@ std::string generate_linear_proto(size_t total_fields) {
     }})";
 
     std::string fields = "";
-    for (int i = 1; i <= total_fields; i++) {
+    for (size_t i = 1; i <= total_fields; i++) {
         fields += std::format("string a{} = {};\n", i, i);
     }
 
@@ -255,7 +252,10 @@ struct counting_consumer {
     }
     ss::future<counting_consumer> end_of_stream() {
         auto res = co_await mux.end_of_stream();
-        BOOST_REQUIRE(!res.has_error());
+        if (res.has_error()) [[unlikely]] {
+            throw std::runtime_error(
+              fmt::format("failed to end stream: {}", res.error()));
+        }
         co_return std::move(*this);
     }
 };
@@ -343,7 +343,10 @@ private:
         }
 
         auto reg_res = co_await _record_gen.register_avro_schema(name, schema);
-        BOOST_REQUIRE(reg_res.has_value());
+        if (reg_res.has_error()) [[unlikely]] {
+            throw std::runtime_error(fmt::format(
+              "failed to register avro schema: {}", reg_res.error()));
+        }
     }
 
     ss::future<>
@@ -355,7 +358,10 @@ private:
 
         auto reg_res = co_await _record_gen.register_protobuf_schema(
           name, schema);
-        BOOST_REQUIRE(reg_res.has_value());
+        if (reg_res.has_error()) [[unlikely]] {
+            throw std::runtime_error(fmt::format(
+              "failed to register protobuf schema: {}", reg_res.error()));
+        }
     }
 
     ss::future<chunked_vector<model::record_batch>> generate_batches(
@@ -377,7 +383,10 @@ private:
                 auto res = co_await add_batch(batch_builder);
                 ++o;
 
-                BOOST_REQUIRE(!res.has_error());
+                if (res.has_error()) [[unlikely]] {
+                    throw std::runtime_error(
+                      fmt::format("unable to add record: {}", res.error()));
+                }
             }
             auto batch = std::move(batch_builder).build();
             ret.emplace_back(std::move(batch));
